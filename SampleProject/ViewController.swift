f04ad3f084e47ecc56e271d4fcfe667037b38924
tanaka.kenji <tanaka.kenji@classmethod.jp>
2018-01-26 17:32:04 +0900

    Initial Commit

---
+++ SampleProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SampleProject
+//
+//  Created by tanaka.kenji on 2018/01/26.
+//  Copyright © 2018年 tanaka.kenji. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
